2a3c35b44c3e19486d9275fdc31626209aa5f05b
tomludden <148908140+tomludden@users.noreply.github.com>
2025-03-23 18:23:57 +0100

    game array changes

--- src/components/gameArray.js
+++ src/components/gameArray.js
@@ -1,12 +1,12 @@
 export const gameArray = [
   {
-    link: 'https://dainty-hamster-1bad5e.netlify.app/',
+    link: 'https://iridescent-fudge-6837ba.netlify.app/',
     imageSrc: './src/assets/images/game1.png',
     description: '3 in a row - the classic game to play with friends',
     alt: 'Project 1'
   },
   {
-    link: 'https://stunning-crisp-2de041.netlify.app/',
+    link: 'https://gorgeous-malabi-518ff1.netlify.app/',
     imageSrc: './src/assets/images/game2.png',
     description:
       'Yummy Fruits - can you collect as many fruits as possible while avoiding the tempting sweets?',
@@ -14,7 +14,7 @@ export const gameArray = [
   },
 
   {
-    link: 'https://heroic-kelpie-b0481e.netlify.app/',
+    link: 'https://velvety-pudding-ca9ca4.netlify.app/',
     imageSrc: './src/assets/images/game3.png',
     description:
       'Match It - match as many pairs as you can...are you up to the challenge?',
