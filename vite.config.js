e114cc11005a10dc8f85dcdb8d98499e130a27dc
tomludden <148908140+tomludden@users.noreply.github.com>
2025-04-10 01:11:35 +0200

    .

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  root: './toms-game-hub', // Points to the folder containing your index.html
-  build: {
-    outDir: '../dist' // Ensures built files are output in the correct location
-  }
-})
