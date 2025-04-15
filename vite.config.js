6a00783aae569065718b319f1a02a6c0faad4edf
tomludden <148908140+tomludden@users.noreply.github.com>
2025-04-15 13:41:59 +0200

    !

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  base: './', // Ensures relative paths work
-  build: {
-    outDir: 'dist', // Ensure correct output folder
-    assetsDir: 'assets' // Keeps assets organized in 'dist/assets'
-  }
-})
